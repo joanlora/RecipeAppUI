a6d4adf0e6cc8880c62aa638bcb0a3c72eca10ca
Lora <a744002@fmr.com>
2023-02-02 16:15:19 -0500

    Learning Angular!

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+
+const routes: Routes = [
+  { path: '', component: RecipeListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+
+  constructor(){}
+
+  // TODO: will be replaced by actual backend data later from Spring
+  recipes = [
+    {id: 1, name: 'Chocolate Chip Cookies', description: 'Sugar, flour, chocolate chips, etc.'},
+    {id: 2, name: 'Wheat Bread', description: 'Yeast,flour, water, etc.'},
+    {id: 3, name: 'Apple Pie', description: 'Apple pie filling, pie crust, water, etc.'}
+  ];
+
+  selectedRecipe?: any;
+
+  ngOnInit(): void{}
+
+}
