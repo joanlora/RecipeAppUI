abbfc246502f49173d1dc61fc8390f19376d1d1e
Lora <Joan.Lora@outlook.com>
2023-02-03 12:07:22 -0500

    Add SpringBoot calls to Angular Components

--- src/app/recipe-form/recipe-form.component.ts
+++ src/app/recipe-form/recipe-form.component.ts
@@ -1,22 +1,45 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-form',
   templateUrl: './recipe-form.component.html',
   styleUrls: ['./recipe-form.component.css'],
 })
-export class RecipeFormComponent {
-  model = { id: null, name: null, description: null };
-  constructor(private router: Router) {}
+export class RecipeFormComponent implements OnInit {
+  model: any;
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    if (this.route.snapshot.paramMap.get('id') != null)
+      this.recipeService
+        .getRecipe(this.route.snapshot.paramMap.get('id'))
+        .subscribe({
+          next: (r) => (this.model = r),
+          error: (e) => console.log(e),
+        });
+    else {
+      this.model = { id: null, name: null, description: null };
+    }
+  }
 
   onSubmit(): void {
     if (this.model.id) {
-      // save a new recipe
+      this.recipeService.updateRecipe(this.model.id, this.model).subscribe({
+        next: (r) => this.router.navigate(['/']),
+        error: (e) => console.error(),
+      });
     } else {
-      // update existing recipe
+      this.recipeService.createRecipe(this.model).subscribe({
+        next: (r) => this.router.navigate(['/']),
+        error: (e) => console.error(),
+      });
     }
 
     this.router.navigate(['/']);
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,32 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
 
-  constructor(){}
+  constructor(private recipeService: RecipeService){}
 
-  // TODO: will be replaced by actual backend data later from Spring
-  recipes = [
-    {id: 1, name: 'Chocolate Chip Cookies', description: 'Sugar, flour, chocolate chips, etc.'},
-    {id: 2, name: 'Wheat Bread', description: 'Yeast,flour, water, etc.'},
-    {id: 3, name: 'Apple Pie', description: 'Apple pie filling, pie crust, water, etc.'}
-  ];
+  recipes : any;
 
   selectedRecipe?: any;
 
-  ngOnInit(): void{}
+  ngOnInit(): void{
+    this.recipeService.getRecipeList().subscribe({next:r => this.recipes = r, error: e => console.log(e)});
+  }
 
   onSelect(recipe: any): void{
     this.selectedRecipe = recipe
   }
 
   onDelete(recipe:any): void{
-    this.recipes = this.recipes.filter(obj => obj.id != recipe.id);
-    // TODO: Call backend delete and list refresh here and remove line aboves
+    this.recipeService.deleteRecipe(recipe.id).subscribe({next:r => this.recipes = r, error: e => console.log(e)});
   }
 
 }
