6ba6bdcf716a770325b711552eed95bdcb38aa75
Lora <Joan.Lora@outlook.com>
2023-02-03 11:03:21 -0500

    Made Angular call the SpringBoot API

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -7,4 +7,24 @@ import { HttpClient } from '@angular/common/http';
 export class RecipeService {
 
   constructor(private http: HttpClient) { }
+
+  getRecipeList(){
+    return this.http.get<{}[]>('/api/v1/recipes');
+  }
+
+  getRecipe(id:any){
+    return this.http.get(`/api/v1/recipes/ ${id}`);
+  }
+
+  createRecipe(recipe: any){
+    return this.http.post('/api/v1/recipes', recipe);
+  }
+
+  updateRecipe(id: any, recipe: any){
+    return this.http.put(`/api/v1/recipes/${id}`,recipe);
+  }
+
+  deleteRecipe(id: any){
+    return this.http.delete(`/api/v1/recipes/${id}`);
+  }
 }
